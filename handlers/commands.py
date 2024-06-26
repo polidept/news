9b774486d7182373ba9a6eca6a688fdc7c0ee088
Amangeldiev Adilet <adilet206@gmail.com>
2024-06-26 20:31:09 +0600

    redesigned

--- handlers/commands.py
+++ handlers/commands.py
@@ -2,44 +2,41 @@ from aiogram import types
 from aiogram import Router, F
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message, CallbackQuery, FSInputFile, ReplyKeyboardRemove
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.context import FSMContext
 import handlers.keyboard as KB 
+from aiogram.enums import ParseMode
 from parcing import * 
 
 router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message):
-    await message.answer("Welcome to the News Bot!", reply_markup = KB.kb)
-
-@router.message(Command(commands = ["repeat"]))
-async def repeat(message: Message):
-    await message.answer("Starting buttons: ",reply_markup = KB.kb)    
+    await message.answer("Welcome to the News Bot!\nTo get information about bot use /help", reply_markup = KB.kb)
 
 @router.message(Command(commands = ["help"]))
 async def help(message: Message):
     await message.answer(
-        "This bot for recieve articles, descriptions and URLs from news by NYT\n"
+        "<b>This bot for recieve articles, descriptions and URLs from news by NYT</b>\n"
         "\n"
-        "Commands:\n"
+        "<b>Commands:</b>\n"
         "/start - for starting\n"
         "/help - to get information about bot\n"
-        "/repeat - to return to starting buttons\n"
         "----------------------------------------------\n"
-        "Buttons:\n"
-        "Archive - The Archive returns an array of NYT articles for a given month, going back to 1851. Its response fields are the same as the Article Search. The Archive is very useful if you want to build your own database of NYT article metadata. You simply pass the API the year and month and it returns a JSON object with all articles for that month. The Archive response size can be large (~20mb) and isn't meant to be called from the browser.\n"
+        "<b>Buttons:</b>\n"
+        "<b>Archive</b> - The Archive returns an array of NYT articles for a given month, going back to 1851. Its response fields are the same as the Article Search. The Archive is very useful if you want to build your own database of NYT article metadata. You simply pass the API the year and month and it returns a JSON object with all articles for that month. The Archive response size can be large (~20mb) and isn't meant to be called from the browser.\n"
+        "<b>Popular</b> - Returns an array of the most viewed articles on NYTimes.com for specified period of time (1 day, 7 days, or 30 days).\n"
+        "<b>Newswire</b> - With the Times Newswire, you can get links and metadata for Times' articles as soon as they are published on NYTimes.com. The Times Newswire provides an up-to-the-minute stream of published articles. You can filter results by source (all, nyt, inyt) and section (arts, business, ...).\n"
+        "<b>Top_Stories</b> - The Top Stories returns an array of articles currently on the specified section.\n"
         "----------------------------------------------\n"
-        "Popular - Returns an array of the most viewed articles on NYTimes.com for specified period of time (1 day, 7 days, or 30 days).\n"
-        "----------------------------------------------\n"
-        "Newswire - With the Times Newswire, you can get links and metadata for Times' articles as soon as they are published on NYTimes.com. The Times Newswire provides an up-to-the-minute stream of published articles. You can filter results by source (all, nyt, inyt) and section (arts, business, ...).\n"
-        "----------------------------------------------\n"
-        "Top_Stories - The Top Stories returns an array of articles currently on the specified section."
+        "<b>Others:</b>\n"
+        "<b>1</b> - you can write 1 to get interesting news for a day\n"
+        "<b>7</b> - you can write 7 to get interesting news for a week\n"
+        "<b>30</b> - you can write 30 to get interesting news for a month\n",
+        parse_mode=ParseMode.HTML
     )
 
 @router.message(F.text == "Archive")
 async def archive(message: Message):
-    await message.reply("Enter the year and month begin 1940 (for example, 2023 6): ")
+    await message.reply("<b>Enter the year and month begin from 1940</b> <u>(example of query: 2023 6):</u>", parse_mode=ParseMode.HTML)
 
 @router.message(lambda message: message.text.startswith('19') or message.text.startswith('20'))
 async def handle_archive_message(message: Message):
@@ -50,7 +47,7 @@ async def handle_archive_message(message: Message):
         archive_news = get_archive_news(year, month)
         if archive_news:
             for article in archive_news:
-                await message.answer(f"Title: {article['title']}\nDescription: {article['description']}\nURL: {article['url']}")
+                await message.answer(f"<b>Title:</b> {article['title']}\n<b>Description:</b> {article['description']}\n<b>URL:</b> {article['url']}", reply_markup=KB.kb, parse_mode=ParseMode.HTML)
         else:
             await message.answer("Doesn't have any information")
     except ValueError:
@@ -65,24 +62,25 @@ async def popular(message: Message):
 async def popular_1(message: Message):
     popular_news = get_popular_news(1)
     for article in popular_news:
-        await message.answer(f"Title: {article['title']}\nDescription: {article['description']}\nURL: {article['url']}", reply_markup= ReplyKeyboardRemove())
+        await message.answer(f"<b>Title:</b> {article['title']}\n<b>Description:</b> {article['description']}\n<b>URL:</b> {article['url']}",  reply_markup=KB.kb, parse_mode=ParseMode.HTML)
+
     
 @router.message(F.text == "7")
 async def popular_1(message: Message):
     popular_news = get_popular_news(7)
     for article in popular_news:
-        await message.answer(f"Title: {article['title']}\nDescription: {article['description']}\nURL: {article['url']}", reply_markup= ReplyKeyboardRemove())
+        await message.answer(f"<b>Title:</b> {article['title']}\n<b>Description:</b> {article['description']}\n<b>URL:</b> {article['url']}", reply_markup= KB.kb, parse_mode=ParseMode.HTML)
 
 @router.message(F.text == "30")
 async def popular_1(message: Message):
     popular_news = get_popular_news(30)
     for article in popular_news:
-        await message.answer(f"Title: {article['title']}\nDescription: {article['description']}\nURL: {article['url']}", reply_markup= ReplyKeyboardRemove())
+        await message.answer(f"<b>Title:</b> {article['title']}\n<b>Description:</b> {article['description']}\n<b>URL:</b> {article['url']}", reply_markup= KB.kb, parse_mode=ParseMode.HTML)
 
 @router.message(F.text == "Top_Stories")
 async def top_stories(message: Message):
     await message.answer(
-        "Topic sections for Top Stories:\n"
+        "<b>Topic sections for Top Stories:</b>\n"
         "/arts - about arts\n"
         "/automobiles - about automobiles\n"
         "/business - about business\n"
@@ -91,39 +89,40 @@ async def top_stories(message: Message):
         "/science - about science\n"
         "/politics - about politics\n"
         "/technology - about technology\n"
-        "/theatre - about theatre\n"
+        "/theatre - about theatre\n",
+        parse_mode=ParseMode.HTML
     )
 
 @router.message(lambda message: message.text.startswith("/") and not (message.text.startswith("/start") and message.text.startswith("/help")))
 async def section_top_stories(message: Message):
     section = message.text[1:]
-
     top_stories_news = get_top_stories(section)
     for article in top_stories_news:
-        await message.answer(f"Title: {article['title']}\nDescription: {article['description']}\nURL: {article['url']}")
+        await message.answer(f"<b>Title: {article['title']}\n<b>Description:</b> {article['description']}\n<b>URL:</b> {article['url']}", reply_markup=KB.kb, parse_mode=ParseMode.HTML)
 
 @router.message(F.text == "Newswire")
 async def newswire(message: Message):
     await message.answer(
-        "Choose and Enter some options:\n"
+        "<b>Choose and Enter some options:</b>\n"
         "\n"
-        "Sources:\n"
-        "all - items from both The New York Times and The \n"
-        "nyt - New York Times items only \n"
-        "inyt = International New York Times items only (FKA The International Herald Tribune)\n"
+        "<b>Sources:</b>\n"
+        "<b>all</b> - items from both The New York Times and The \n"
+        "<b>nyt</b> - New York Times items only \n"
+        "<b>inyt</b> = International New York Times items only (FKA The International Herald Tribune)\n"
         "\n"
-        "Sections:\n"
-        "arts - about arts\n"
-        "automobiles - about automobiles\n"
-        "business - about business\n"
-        "fasion - about fashion\n"
-        "food - about food\n"
-        "science - about science\n"
-        "politics - about politics\n"
-        "technology - about technology\n"
-        "theatre - about theatre\n"
+        "<b>Sections:</b>\n"
+        "<b>arts</b> - about arts\n"
+        "<b>automobiles</b> - about automobiles\n"
+        "<b>business</b> - about business\n"
+        "<b>fasion</b> - about fashion\n"
+        "<b>food</b> - about food\n"
+        "<b>science</b> - about science\n"
+        "<b>politics</b> - about politics\n"
+        "<b>technology</b> - about technology\n"
+        "<b>theatre</b> - about theatre\n"
         "\n"
-        "Example: newswire nyt all"
+        "<b>Example of query</b>: <u>newswire nyt all</u>", 
+        parse_mode=ParseMode.HTML
     )
 
 @router.message(lambda message: message.text.startswith('newswire'))
@@ -131,4 +130,4 @@ async def chosen_newswire(message: Message):
     command, source, section = message.text.split()
     news = get_times_newswire(source, section)
     for article in news:
-            await message.answer(f"Title: {article['title']}\nDescription: {article['description']}\nURL: {article['url']}")
+            await message.answer(f"<b>Title:</b> {article['title']}\n<b>Description:</b> {article['description']}\n<b>URL:</b> {article['url']}", reply_markup=KB.kb, parse_mode=ParseMode.HTML)
--- handlers/keyboard.py
+++ handlers/keyboard.py
@@ -1,5 +1,5 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardBuilder,InlineKeyboardBuilder
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.types import Message
 
 kb = ReplyKeyboardMarkup(keyboard = [
